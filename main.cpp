b7e292546eda3bce80bddc955dd08be590913ad5
moonfeather <moonbite233@gmail.com>
2024-12-24 10:48:13 +0800

    Add librouter

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "helloworld.h"
+#include "Router.h"
 #include <gtkmm/application.h>
 
 int main(int argc, char* argv[])
 {
     auto app = Gtk::Application::create("org.gtkmm.example");
+
+    auto router = new Router();
+    delete router;
 
     //Shows the window and returns when it is closed.
     return app->make_window_and_run<HelloWorld>(argc, argv);
